eb11576e88fd5c610dbb534e34eb0157e90da26a
lexymochi <beaarcega28@gmail.com>
2024-11-25 00:59:15 +0800

    filename fixed
    renamed c file to have uniform filenames

--- assemble.bat
+++ assemble.bat
@@ -1,5 +1,5 @@
 nasm -f win64 accelerationASM.asm
-gcc -c accelerationMatrix.c -o accelerationC.obj -m64
+gcc -c accelerationC.c -o accelerationC.obj -m64
 gcc accelerationASM.obj accelerationC.obj -o run.exe -m64
 
 run.exe

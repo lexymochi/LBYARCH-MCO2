b4bf85a078ac67ee33395820bb7dfd7bfe1906f1
Cyrus Bautista <136875777+very-vivid-sky@users.noreply.github.com>
2024-11-25 15:13:55 +0800

    accelerationC (new)

--- accelerationC.c
+++ accelerationC.c
@@ -1,6 +1,6 @@
 /*	LBYARCH S17
 	Arcega, Bea Alexis; Bautista, Cyrus Rylie
-	Machine Project 2, accelerationMatrix.c
+	Machine Project 2, accelerationC.c
 	
 	A car's performance is usually measured using an acceleration test
 	with a 1km distance. some cars are measured from a cold start (they
@@ -17,17 +17,33 @@
 	and Time (in Seconds).
 */
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 extern void accelMatrix(int cars, float *matrix, int *resultAccel);
 
+float randomFloat(int max) {
+	return (float)rand() / RAND_MAX + (float)(rand() % max);
+}
+
 int main() {
+	srand(time(NULL));
+	int inputType = -1;
+	printf("Input 0 for manual inputs, 1 for automatic inputs.\n");
+	do {
+		printf("Input type: ");
+		scanf("%d", &inputType);
+	}
+	while (inputType != 0 && inputType != 1);
+	
     int cars;
     float *matrix;
     int *resultAccel;
     int i;
 
+	printf("Number of inputs: ");
     scanf("%d", &cars);
 
     matrix = (float *)malloc(cars * 3 * sizeof(float));
@@ -36,25 +52,61 @@ int main() {
         return 1;
     }
 
-    resultAccel = (int *)malloc(cars * sizeof(int));
-    if (resultAccel == NULL) {
+    resultAccel = (int *)malloc(cars * 3 * sizeof(int));
+    if (matrix == NULL) {
         printf("Allocating memory has failed.");
         free(matrix);
         return 1;
     }
 
-    for (i = 0; i < cars; i++) {
-        scanf("%f %f %f", &matrix[i * 3], &matrix[i * 3 + 1], &matrix[i * 3 + 2]);
-    }
+	if (inputType == 0)
+		for (i = 0; i < cars; i++) {
+			scanf("%f %f %f", &matrix[i * 3], &matrix[i * 3 + 1], &matrix[i * 3 + 2]);
+		}
+	else {
+		for (i = 0; i < cars; i++) {
+			matrix[i*3 + 0] = randomFloat(10000);
+			matrix[i*3 + 1] = randomFloat(10000);
+			matrix[i*3 + 2] = randomFloat(100);
+		}
+	}
+	printf("\n");
+
+
+	// time function
+	clock_t start, end;
+	float timer;
+	start = clock();
 
     accelMatrix(cars, matrix, resultAccel);
 
-    for (i = 0; i < cars; i++) {
-        printf("%d\n", resultAccel[i]);
-    }
+	// stop timer
+	end = clock();
+	timer = (float)(end - start)/CLOCKS_PER_SEC;
 
-    free(matrix);
-    free(resultAccel);
+	int expected;
+	int wrongs = 0;
+	int wrongIndexes[1000];
+	int n;
+	for (i = 0; i < cars; i++) {
+		expected = (int)( roundf((matrix[i*3 + 1]*0.277777791 - matrix[i*3 + 0]*0.277777791) / matrix[i*3 + 2]) );
+		printf("%f > %f at %fkm/h = %d (Expected: %d)", matrix[i*3 + 0], matrix[i*3 + 1], matrix[i*3 + 2], resultAccel[i], expected);
+		if (expected != resultAccel[i]) {
+			printf(" WRONG ANSWER!");
+			wrongIndexes[wrongs] = i;
+			wrongs++;
+		}
+		printf("\n");
+	}
+	
+	printf("\n");
+	printf("There are %d wrong answers.\n", wrongs);
+	for (i = 0; i < wrongs; i++) {
+		n = wrongIndexes[i];
+		expected = (int)( roundf((matrix[n*3 + 1]*0.277777791 - matrix[n*3 + 0]*0.277777791) / matrix[n*3 + 2]) );
+		printf("%f > %f at %fkm/h = %d (Expected: %d)\n", matrix[n*3 + 0], matrix[n*3 + 1], matrix[n*3 + 2], resultAccel[i], expected);
+	}
+	printf("This function took %f ms to run\n", timer*1000);
 
     return 0;
 }
